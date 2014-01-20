f6aafbe0d293fef9de948742eb23175240f1ef23
Miguel Angel Gabriel <magabriel@gmail.com>
2014-01-20 11:40:41 +0100

    Added license header to PHP files

--- src/Trefoil/Plugins/BasePlugin.php
+++ src/Trefoil/Plugins/BasePlugin.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of the trefoil application.
+ *
+ * (c) Miguel Angel Gabriel <magabriel@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace Trefoil\Plugins;
 
 use Easybook\Events\BaseEvent;
--- src/Trefoil/Tests/bootstrap.php
+++ src/Trefoil/Tests/bootstrap.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of the trefoil application.
+ *
+ * (c) Miguel Angel Gabriel <magabriel@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 require_once __DIR__ . '/../../../vendor/autoload.php';
 
 // TODO: this is a temporary fix.
