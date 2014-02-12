bc50129b0994b5a74ce5ce0284fb48d0dec8cf60
Miguel Angel Gabriel <magabriel@gmail.com>
2014-02-12 17:12:27 +0100

    SensioLabsInsight sugested changes

--- src/Trefoil/Bridge/Easybook/RegisterResources.php
+++ src/Trefoil/Bridge/Easybook/RegisterResources.php
@@ -12,7 +12,6 @@ namespace Trefoil\Bridge\Easybook;
 
 use Easybook\Events\BaseEvent;
 use Easybook\Events\EasybookEvents;
-use Symfony\Component\Console\Application;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Finder\Finder;
 use Trefoil\Events\TrefoilEvents;
